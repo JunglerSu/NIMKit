30ab283c07bcc488b30fd5a0219acb5ccb3e6d0c
JunglerSu <yunyi@yundeMac-mini.local>
2019-07-10 16:47:58 +0800

    创建项目

---
+++ NIMKitDemo/NIMKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NIMKitDemo
+//
+//  Created by yun yi on 2019/7/10.
+//  Copyright © 2019 yun yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NIMKitDemo/NIMKitDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  NIMKitDemo
+//
+//  Created by yun yi on 2019/7/10.
+//  Copyright © 2019 yun yi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
